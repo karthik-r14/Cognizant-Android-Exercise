90805d1df0fbec782a88cce03ac98faac70aa8fb
Karthik R <karthikr1493@gmail.com>
2020-03-05 19:51:50 +0530

    Add Butterknife Dependency to build.gradle.

--- app/src/main/java/com/cognizant_android_exercise/view/MainActivity.java
+++ app/src/main/java/com/cognizant_android_exercise/view/MainActivity.java
@@ -1,10 +1,13 @@
 package com.cognizant_android_exercise.view;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.cognizant_android_exercise.R;
 
 import androidx.appcompat.app.AppCompatActivity;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity implements MainView {
 
@@ -12,5 +15,11 @@ public class MainActivity extends AppCompatActivity implements MainView {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.refresh_button)
+    public void onRefreshButtonClick(View view) {
+
     }
 }
